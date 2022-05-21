888c8e767b6b96ec65a69e3f0f08b53cd1d79a1f
Nayuta <naayudan@gmail.com>
2022-05-20 21:35:05 -0300

    Fragment

---
+++ ReactJS/advanced-reactjs-dio/src/App.js
@@ -0,0 +1,28 @@
+import React, { Fragment } from "react";
+
+const store = ['', '', '']
+
+function Column() {
+    return (
+        <tr>
+            <td>Tenis</td>
+            <td>Roupa</td>
+        </tr>
+    )
+}
+
+function App() {
+    const renderColumns = (element, key) => (
+        <Fragment key={`column-${key}`}>
+            <Column />
+        </Fragment>
+    )
+
+    return (
+        <table>
+            {store.map(renderColumns)}
+        </table>
+    )
+}
+
+export default App
